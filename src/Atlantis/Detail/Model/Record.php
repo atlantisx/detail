bb9a77e64a840e92aa12b854cfdd462fa25a5615
Azri Jamil <azri@nematix.com>
2014-05-14 16:57:22 +0800

    Move Record model  non essential function, override to module Advance

--- src/Atlantis/Detail/Model/Record.php
+++ src/Atlantis/Detail/Model/Record.php
@@ -7,9 +7,10 @@ use Rhumsaa\Uuid\Uuid;
 class Record extends Eloquent {
     protected $table = 'details';
     protected $primaryKey = 'uuid';
-    protected $guarded = array('uuid','message','user','guardian','guarantor','url_update','application','status_title','status_label','url_update','created_when','updated_when');
-    protected $appends = array('status_title','status_label','url_update','created_when','updated_when');
+    protected $guarded = array('uuid','created_when','updated_when');
+    protected $appends = array('created_when','updated_when');
     public $incrementing = false;
+
 
     protected static function boot()
     {
@@ -26,12 +27,7 @@ class Record extends Eloquent {
 
 
     public function detail(){
-        return $this->belongsTo('Atlantis\Detail\Model\Detail');
-    }
-
-
-    public function user(){
-        return $this->belongsTo('User');
+        return $this->belongsTo('Detail','application_id','id');
     }
 
 
@@ -40,23 +36,13 @@ class Record extends Eloquent {
     }
 
 
-    public function guardian(){
-        return $this->hasOne('People','id','guardian_id');
+    public function getMetaAttribute($value){
+        if( empty($value) ) $value = '{}';
+        return json_decode($value);
     }
 
-
-    public function guarantor(){
-        return $this->hasOne('People','id','guarantor_id');
-    }
-
-
-    public function getStatusTitleAttribute(){
-        return $this->status_title();
-    }
-
-
-    public function getStatusLabelAttribute(){
-        return $this->status_label();
+    public function setMetaAttribute($value){
+        $this->attributes['meta'] = json_encode($value);
     }
 
 
@@ -67,24 +53,6 @@ class Record extends Eloquent {
 
     public function getUpdatedWhenAttribute(){
         return \Carbon\Carbon::createFromTimeStamp(strtotime($this->updated_at))->diffForHumans();
-    }
-
-
-    public function getUrlUpdateAttribute(){
-        #i: Check for application name from detail
-        $application_name = ($this->application ? $this->application->name : 'default');
-
-        #i: Return the update url
-        return action('\Modules\Advance\ApplicationController@getUpdate',array($application_name,$this->uuid));
-    }
-
-
-    public function getMetaAttribute($value){
-        if( empty($value) ) $value = '{}';
-        return json_decode($value);
-    }
-    public function setMetaAttribute($value){
-        $this->attributes['meta'] = json_encode($value);
     }
 
 
@@ -127,55 +95,8 @@ class Record extends Eloquent {
         });
     }
 
-    protected function status_title($status=null){
-        if( !isset($status) ) $status = $this->status;
 
-        //[i] Get general status
-        $status_title = trans( 'advance.status.general.'.$status);
-
-        //[i] Declared status group for student
-        $status_student = array(
-            1 => array(1,2,3,4,5,6),
-            2 => array(7),
-            3 => array(8)
-        );
-
-        //[i] Get diff status if role is student
-        $user = \Sentry::getUser();
-        if( $user ){
-            $user_realm = \App::make('atlantis.realm')->byUserId($user->id);
-
-            if($user_realm->name == 'student'){
-                foreach( $status_student as $key => $value ){
-                    if( in_array($status,$value) ){
-                        $status_title = trans( 'advance.status.student.'.$key);
-                        break;
-                    }
-                }
-            }
-        }
-
-        return $status_title;
-    }
-
-
-    public static function status_count($statuses){
-        $status_count = array();
-
-        foreach($statuses as $status){
-            $status_count[$status] = Record::where('status','=',$status)->count();
-        }
-
-        return $status_count;
-    }
-
-
-    protected function status_label($status=null){
-        if( !isset($status) ) $status = $this->status;
-        return $status_title = trans( 'advance.status_label.'.$status);;
-    }
-
-    protected function progress_percent(){
+    /*protected function progress_percent(){
         $incomplete = array();
         $required = array(
             $this->institution_name,             //institusi
@@ -204,5 +125,5 @@ class Record extends Eloquent {
         }
 
         return round( ( count($incomplete) / count($required) ) * 100 );
-    }
+    }*/
 }

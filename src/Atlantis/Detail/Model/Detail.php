d6df91063b777d7b0b4e073e6e488f0067746b8a
Azri Jamil <azri@nematix.com>
2014-05-17 00:36:21 +0800

    Refactor contextReaction into separate task

--- src/Atlantis/Detail/Model/Detail.php
+++ src/Atlantis/Detail/Model/Detail.php
@@ -59,25 +59,34 @@ class Detail extends Eloquent {
             $value_original = parent::getAttribute($key);
         }
 
-        #i: Get context
-        $contexts = \App::make('context');
-
-        #i: Filter contexts
-        $context = Arrays::find($contexts->all(), function($value){
-            return $value->reaction_parameters->model == get_called_class();
-        });
-
-        if($context){
-            #i: Inspect context
-            $value_override = $contexts->reactionInspect($context->reaction_provider,[$this,$key,$context->reaction_parameters]);
-
-            #i: Override value
-            if($value_override) $value_original = $value_override;
-        }
+        #i: Context Reaction
+        $context_value = $this->getContextReaction($key);
+        if( $context_value ) $value_original = $context_value;
 
         return $value_original;
     }
 
+
+    public function getContextReaction($key){
+        #i: Get context
+        $contexts = \App::make('context');
+
+        #i: Filter Reaction Contexts
+        $context = Arrays::find($contexts->all(), function($value){
+            return $value->reaction_parameters->model == get_called_class();
+        });
+
+        #i: Inspect reaction context
+        if($context){
+            #i: Inspect context
+            $value = $contexts->reactionInspect($context->reaction_provider,[$this,$key,$context->reaction_parameters]);
+
+            #i: Override value
+            if($value) return $value;
+        }
+
+        return null;
+    }
 
     public function __isset($key){
         if( $this->getAttribute($key) ) return true;
